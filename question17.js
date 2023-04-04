3208b52c3c04efd0f529d4b90f5929053ccb4cff
sang woo <tooil1729@gmail.com>
2023-04-04 13:27:16 +0900

    [JS] 17번 문제 해석

---
+++ question17.js
@@ -0,0 +1,29 @@
+
+// 배열 원소의 길이
+/*
+문제 설명
+문자열 배열 strlist가 매개변수로 주어집니다. strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ strlist 원소의 길이 ≤ 100
+strlist는 알파벳 소문자, 대문자, 특수문자로 구성되어 있습니다.
+
+입출력 예 #1
+
+["We", "are", "the", "world!"]의 각 원소의 길이인 [2, 3, 3, 6]을 return합니다.
+
+입출력 예 #2
+
+["I", "Love", "Programmers."]의 각 원소의 길이인 [1, 4, 12]을 return합니다.
+*/
+
+/*
+배열을 순회하면서 나온 값들을 차례대로 길이로 변환하여, 새로운 배열에 담아 리턴한다.
+배열 순회에는 foreach, map, sort 등이 있다.
+map 메소드는 반환시 새로운 배열에 담아 반환해준다.
+*/
+
+function solution(strlist) {
+
+}
+solution(["we", 'aer', "the", 'world!']);
