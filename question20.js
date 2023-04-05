e009c9b86176358a93205e1f8e5e097352011be6
sang woo <tooil1729@gmail.com>
2023-04-05 12:21:14 +0900

    [JS] 20번 문제 해석

---
+++ question20.js
@@ -0,0 +1,29 @@
+
+// 문자열 뒤집기
+/*
+문제 설명
+문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ my_string의 길이 ≤ 1,000
+
+입출력 예 #1
+
+my_string이 "jaron"이므로 거꾸로 뒤집은 "noraj"를 return합니다.
+
+입출력 예 #2
+
+my_string이 "bread"이므로 거꾸로 뒤집은 "daerb"를 return합니다.
+*/
+
+/*
+reverse 메서드가 먼저 생각이 날 수 있지만 reverse 메서드는 배열의 메서드 이므로
+편하게 문자열을 뒤집을 수는 없다. 그래서 문자열을 배열로 만든 후에 뒤집어서 문자열로 반환하는
+방법으로 접근한다. 
+*/
+
+
+function solution(my_string) {
+  
+}
+solution()
